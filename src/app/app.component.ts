9d346bcfd9b2c2662dfb4509c4181ec4a6346e8e
eddgerig <edgareduardogerig@gmail.com>
2025-02-19 18:18:22 -0400

    SideNav User

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SidenavComponent } from './side-nav/side-nav.component';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent, SidenavComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
