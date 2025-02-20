84312210a81717441f4a817a57d88ad2c4616e80
eddgerig <edgareduardogerig@gmail.com>
2025-02-20 18:38:36 -0400

    a;adiendo component sidenav en app.component para probar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from './side-nav/side-nav.component';
 
 
 
@@ -7,7 +8,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
