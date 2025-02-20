937d5976dda7b9c1e08a8dfbdcb7fcc3ccfe751a
eddgerig <edgareduardogerig@gmail.com>
2025-02-20 18:52:19 -0400

    front login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 
 
 export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
+
 
 ];
