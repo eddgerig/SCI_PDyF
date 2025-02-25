b92d3317eba75070fdabaaf843aeb461b3f16e3c
pebelinSystem <pebelin.hernandez@pegasoconsulting.net>
2025-02-25 12:11:27 -0400

    creacion del modelo user

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,26 @@
+export class IUser{
+
+    id : number;
+    nombres: string;
+    apellidos: string;
+    cedula: number;
+    correo: string;
+    //telefono: number;
+    contrasena: string;
+    rol: number;
+
+    constructor(){
+        this.id = 0;
+        this.nombres = '';
+        this.apellidos = '';
+        this.cedula = 0;
+        this.correo = '';
+        //this.telefono = 0;
+        this.contrasena = '';
+        this.rol = 0;
+    }
+
+    private iniciar_session(value: number){
+        return value == 0 ? false : true;
+    }
+}
