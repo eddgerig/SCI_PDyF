0e3ec0b96e1ca215de01978580c8e1af119b4d01
eddgerig <edgareduardogerig@gmail.com>
2025-02-26 12:30:38 -0400

    Directiva NgIf basada en roles para el sidenav component

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
+import { UsuarioBdService } from '../service/usuario-bd.service';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
   imports: [CommonModule, RouterModule],
-    templateUrl: './side-nav.component.html',
-    styleUrl: './side-nav.component.css'
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.css']
 })
-export class SidenavComponent {
-  constructor(private router: Router) { }
+export class SidenavComponent implements OnInit {
+  rol: number | null = null; // Almacenaremos el rol del usuario aquí
+
+  constructor(private router: Router, private usuarioBdService: UsuarioBdService) { }
+
+  ngOnInit(): void {
+    const usuario = 'nombreUsuario'; // Aquí puedes obtener el nombre de usuario de donde lo necesites, como del almacenamiento local, sesión, etc.
+    this.usuarioBdService.obtenerRolUsuario(usuario, (rol) => {
+      this.rol = rol; // Guardamos el rol obtenido
+      console.log('Rol del usuario:', this.rol);
+    });
+  }
+
   logout() {
-    // Implement logout logic here
+    // Implementar la lógica de cierre de sesión
     this.router.navigate(['/login']);
     console.log('Logging out...');
   }
