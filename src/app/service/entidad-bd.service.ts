d697a48104f12b36b6d037949100aed002d21295
eddgerig <edgareduardogerig@gmail.com>
2025-03-01 16:52:32 -0400

    registrar y actualizar entidad

--- src/app/service/entidad-bd.service.ts
+++ src/app/service/entidad-bd.service.ts
@@ -65,4 +65,47 @@ export class EntidadBdService {
 
     return this.entidadesSubject.asObservable();
   }
+
+
+  public actualizarEntidad(
+    id: number, // Identificador único de la entidad
+    tipo_brecha: string,
+    tipo_proyecto: string,
+    procesos_corregidos: string,
+    procesos_realizados: string,
+    investigadores: string,
+    empresas: string,
+    subtipo_ficha: string,
+    tipo_irregularidad: string,
+    subtipo_irregularidad: string,
+    procedencia_casos: string
+  ) {
+    console.log("Actualizando entidad con ID desde service:", id);
+    (window as any).entidadesAPI.actualizarEntidad(
+      id,
+      tipo_brecha,
+      tipo_proyecto,
+      procesos_corregidos,
+      procesos_realizados,
+      investigadores,
+      empresas,
+      subtipo_ficha,
+      tipo_irregularidad,
+      subtipo_irregularidad,
+      procedencia_casos
+    );
+
+    // Escuchar la respuesta de la actualización
+    (window as any).entidadesAPI.onEntidadActualizada((event: any, response: any) => {
+      if (response.error) {
+        console.error('Error al actualizar entidad:', response.error);
+      } else {
+        console.log('Entidad actualizada con éxito:', response);
+      }
+    });
+  }
+
+
+
+
 }
