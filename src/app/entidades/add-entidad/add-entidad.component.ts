f6848fd4455f482410b8102659a92172ba75065a
eddgerig <edgareduardogerig@gmail.com>
2025-03-01 11:51:04 -0400

    listo editar entidad

--- src/app/entidades/add-entidad/add-entidad.component.ts
+++ src/app/entidades/add-entidad/add-entidad.component.ts
@@ -1,9 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
 import { SidenavComponent } from '../../side-nav/side-nav.component';
 import { TabEntidadesComponent } from '../tab-entidades/tab-entidades.component';
 import { FormEntidadComponent } from '../form-entidad/form-entidad.component';
 import { IEntidad } from '../../models/entidad.model';
 import { FormBuilder } from '@angular/forms';
+import { EntidadBdService } from '../../service/entidad-bd.service';
 @Component({
   selector: 'app-add-entidad',
   standalone: true,
@@ -14,7 +15,11 @@ import { FormBuilder } from '@angular/forms';
 export class AddEntidadComponent {
    @Input()entidadSelected : IEntidad = new IEntidad;
      @Output() goBack = new EventEmitter<void>();
-         constructor(private fb: FormBuilder){/*this.loadForm();*/}
+     showAddEntidad = false; // Variable de estado 
+         constructor(private fb: FormBuilder,
+                          private cdr: ChangeDetectorRef ,
+                            private entidadService: EntidadBdService
+         ){/*this.loadForm();*/}
          ngOnInit() {
            console.log("Entidad seleccionada desde AddEntidad", this.entidadSelected)
          }
@@ -22,5 +27,12 @@ export class AddEntidadComponent {
           console.log("onGoBack")
           this.goBack.emit(); // Emitir el evento
         }
+
+        hideAddEntidadForm() {
+          this.showAddEntidad = false;
+          this.onGoBack();
+          this.cdr.detectChanges();
+        }
+  
 
 }
--- src/app/entidades/form-entidad/form-entidad.component.ts
+++ src/app/entidades/form-entidad/form-entidad.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule, NgIf } from '@angular/common';
 import { ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IEntidad } from '../../models/entidad.model';
 import { EntidadBdService } from '../../service/entidad-bd.service';
 @Component({
@@ -22,13 +22,49 @@ constructor(
     
   
   ) {
-    //this.loadForm();
+    this.loadForm();
   }
 
   ngOnInit() {
     console.log("Form Entidad Component", this.entidadSelected);
+    if(!this.entidadSelected ){
+              this.entidadSelected = new IEntidad();
+            }
+    this.setForm();
   }
 
+  loadForm(): void {
+      this.entidadForm = this.fb.group({
+        id: [null],
+        tipo_brecha: [null],
+        tipo_proyecto: [null],
+        procesos_corregidos: [null],
+        procesos_realizados: [null],
+        investigadores: [null],
+        empresas: [null],
+        subtipo_ficha: [null],
+        tipo_irregularidad: [null],
+        subtipo_irregularidad: [null],
+        procedencia_casos: [null]
+      });
+    }
+
+    setForm(): void {
+      this.entidadForm.reset({
+        id: this.entidadSelected.id,
+        tipo_brecha: this.entidadSelected.tipo_brecha,
+        tipo_proyecto: this.entidadSelected.tipo_proyecto,
+        procesos_corregidos: this.entidadSelected.procesos_corregidos,
+        investigadores: this.entidadSelected.investigadores,
+        empresas: this.entidadSelected.empresas,
+        subtipo_ficha:this.entidadSelected.subtipo_ficha,
+        tipo_irregularidad: this.entidadSelected.tipo_irregularidad,
+        subtipo_irregularidad: this.entidadSelected.subtipo_irregularidad,
+        procedencia_casos: this.entidadSelected.procedencia_casos
+
+      });
+    }
+  
 
   onGoBack() {
     console.log("onGoBack")
