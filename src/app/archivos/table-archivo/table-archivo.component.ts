31dad226668f0b4562a8fc69b6bba2f8598ce7df
eddgerig <edgareduardogerig@gmail.com>
2025-03-02 18:37:00 -0400

    listo ver archivos desde un alert

--- src/app/archivos/table-archivo/table-archivo.component.ts
+++ src/app/archivos/table-archivo/table-archivo.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ArchivosService } from '../../service/archivos.service';
-import { Archivo } from '../../models/archivosFactoryMethod.model';
+import { Amonestado, Archivo, SerialEquiposRobados } from '../../models/archivosFactoryMethod.model';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -44,18 +44,38 @@ export class TableArchivoComponent {
 
   verArchivo(archivo: Archivo): void {
     console.log('Ver archivo:', archivo);
-    
+    //this.onRowSelect(archivo);
   
-    // Construir el mensaje para el alert
-    const mensaje = `
-      ID Archivo: ${archivo.id_archivo}
-      
-      Tipo: ${archivo.tipo}
-      Descripción: ${archivo.descripcion}
-      
-      
-    `;
-    
+    let mensaje: string;
+  
+    // Verificar el tipo de archivo usando la propiedad "tipo"
+    if (archivo.tipo === 'SerialEquiposRobados') {
+      const archivoEspecifico = archivo as SerialEquiposRobados; // Type casting
+      mensaje = `
+        ID Archivo: ${archivoEspecifico.id_archivo}
+        Caso: ${archivoEspecifico.caso}
+        Tipo: ${archivoEspecifico.tipo}
+        Descripción: ${archivoEspecifico.descripcion}
+        Serial: ${archivoEspecifico.serial}
+        Tipo de Equipo: ${archivoEspecifico.tipoEquipo}
+        Modelo: ${archivoEspecifico.modelo}
+        Observaciones: ${archivoEspecifico.observaciones}
+      `;
+    } else if (archivo.tipo === 'Amonestado') {
+      const archivoEspecifico = archivo as Amonestado; // Type casting
+      mensaje = `
+        ID Archivo: ${archivoEspecifico.id_archivo}
+        Caso: ${archivoEspecifico.caso}
+        Tipo: ${archivoEspecifico.tipo}
+        Descripción: ${archivoEspecifico.descripcion}
+        Cédula: ${archivoEspecifico.cedula}
+        Nombre: ${archivoEspecifico.nombre}
+        Apellido: ${archivoEspecifico.apellido}
+        Empresa: ${archivoEspecifico.empresa}
+      `;
+    } else {
+      mensaje = 'Tipo de archivo no soportado';
+    }
   
     // Mostrar el alert con la información del archivo
     alert(mensaje);
